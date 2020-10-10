e8e56413e17c9425686b8463b34e10aef242926a
Sebastian Aguirre <sebastian.aguirre@sofka.com.co>
2020-10-10 13:32:50 -0500

    correccion tipos de datos attrs

--- Lib_Parcial2/Lib_Parcial2/lib/ReglaDeNegocio.cs
+++ Lib_Parcial2/Lib_Parcial2/lib/ReglaDeNegocio.cs
@@ -5,19 +5,33 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Lib_Parcial2.lib
-{
+{ 
     public class ReglaDeNegocio
     {
-        public String programa { set; get; }
-        public String estratoColegio { set; get; }
-        public String estratoVivienda { set; get; }
-        public int numeroCreditos { set; get; }
-        public double porcentajeDesuento { set; get; }
-        public String error { set; get; }
 
-        public bool obtenerPorcentajeDescuento() {
+        #region Constructor
+        public ReglaDeNegocio()
+        {
+
+        }
+        #endregion
+
+        #region Atributos/Propiedades
+        public string programa { set; get; }
+        public int estratoColegio { set; get; }
+        public int estratoVivienda { set; get; }
+        public double porcentajeDescuento { private set; get; }
+        public string error { private set; get; }
+        #endregion
+
+
+        #region Metodos
+        public bool obtenerPorcentajeDescuento()
+        {
             porcentajeDesuento = 0.2;
             return true;
         }
+        #endregion
+
     }
 }
