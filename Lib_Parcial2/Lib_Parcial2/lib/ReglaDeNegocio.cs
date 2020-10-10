1a0757ae0d97b90f87cbbd87c084678ef907ff58
John Santa <andersonsanta3@gmail.com>
2020-10-10 13:11:14 -0500

    punto de partida

---
+++ Lib_Parcial2/Lib_Parcial2/lib/ReglaDeNegocio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib_Parcial2.lib
+{
+    public class ReglaDeNegocio
+    {
+        public String programa { set; get; }
+        public String estratoColegio { set; get; }
+        public String estratoVivienda { set; get; }
+        public int numeroCreditos { set; get; }
+        public double porcentajeDesuento { set; get; }
+        public String error { set; get; }
+
+        public bool obtenerPorcentajeDescuento() {
+            porcentajeDesuento = 0.2;
+            return true;
+        }
+    }
+}
