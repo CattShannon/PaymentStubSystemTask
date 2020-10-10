3b5c75cfab6ae938d705c4b0b1531567edd62f0d
John Santa <andersonsanta3@gmail.com>
2020-10-10 12:31:03 -0500

    creacion proyecto

---
+++ WebApplication1/WebApplication1/Parcial2/Front.aspx
@@ -0,0 +1,73 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Site.Master" CodeBehind="Front.aspx.vb" Inherits="WebApplication1.Front" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+        <table class="nav-justified">
+            <tr>
+                <td class="text-center" colspan="2"><strong><span style="font-size: xx-large">LIQUIDACIÓN MATRÍCULA</span></strong>&nbsp;</td>
+            </tr>
+            <tr>
+                <td style="font-size: x-large">P<span style="font-weight: bold">ROGRAMA:</span></td>
+                <td class="text-center">
+                    <asp:DropDownList ID="cboPrograma" runat="server" style="font-size: x-large">
+                        <asp:ListItem>INGENIERÍA SISTEMAS</asp:ListItem>
+                        <asp:ListItem>INGENIERÍA ELECTRÓNICA</asp:ListItem>
+                        <asp:ListItem>OTROS</asp:ListItem>
+                    </asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td style="font-size: x-large"><strong>ESTRATO VIVIENDA:</strong></td>
+                <td class="text-center">
+                    <asp:TextBox ID="txtEstratoVivienda" runat="server" style="font-size: x-large" TextMode="Number"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td style="font-size: x-large"><strong>ESTRATO COLEGIO:</strong></td>
+                <td class="text-center">
+                    <asp:TextBox ID="txtEstratoColegio0" runat="server" style="font-size: x-large" TextMode="Number"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td style="font-size: x-large"><strong>CRÉDITOS:</strong></td>
+                <td class="text-center">
+                    <asp:TextBox ID="txtCreditos" runat="server" style="font-size: x-large" TextMode="Number"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="text-center">
+                    <asp:Button ID="btnCalcular" runat="server" BackColor="#0033CC" Font-Bold="True" ForeColor="White" OnClick="btnCalcular_Click" style="font-size: xx-large" Text="CALCULAR" Width="280px" />
+                </td>
+                <td class="text-center">
+                    <asp:Button ID="btnLimpiar" runat="server" BackColor="#0033CC" Font-Bold="True" ForeColor="White" style="font-size: xx-large" Text="LIMPIAR" Width="280px" />
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <asp:Label ID="lblError" runat="server" style="font-size: x-large"></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td style="font-size: x-large"><span style="font-weight: bold">Valor pago antes descuento:</span></td>
+                <td class="text-center">
+                    <asp:Label ID="lblPorcentajeCuotaInicial" runat="server" style="font-size: x-large"></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td style="font-size: x-large"><span style="font-weight: bold">Porcentaje descuento:</span></td>
+                <td class="text-center">
+                    <asp:Label ID="lblValorCuotaInicial" runat="server" style="font-size: x-large"></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td style="font-size: x-large"><span style="font-weight: bold">Valor descuento:</span></td>
+                <td class="text-center">
+                    <asp:Label ID="lblCredito" runat="server" style="font-size: x-large"></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td style="font-size: x-large"><strong>T</strong><span style="font-weight: bold">otal a pagar:</span></td>
+                <td class="text-center">
+                    <asp:Label ID="lblTasa" runat="server" style="font-size: x-large"></asp:Label>
+                </td>
+            </tr>
+            </table>
+</asp:Content>
---
+++ WebApplication1/WebApplication1/Parcial2/Front.aspx.designer.vb
@@ -0,0 +1,114 @@
+﻿'------------------------------------------------------------------------------
+' <generado automáticamente>
+'     Este código fue generado por una herramienta.
+'
+'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+'     se vuelve a generar el código. 
+' </generado automáticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class Front
+
+    '''<summary>
+    '''Control cboPrograma.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents cboPrograma As Global.System.Web.UI.WebControls.DropDownList
+
+    '''<summary>
+    '''Control txtEstratoVivienda.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtEstratoVivienda As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control txtEstratoColegio0.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtEstratoColegio0 As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control txtCreditos.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtCreditos As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control btnCalcular.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents btnCalcular As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''Control btnLimpiar.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents btnLimpiar As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''Control lblError.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lblError As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control lblPorcentajeCuotaInicial.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lblPorcentajeCuotaInicial As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control lblValorCuotaInicial.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lblValorCuotaInicial As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control lblCredito.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lblCredito As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control lblTasa.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lblTasa As Global.System.Web.UI.WebControls.Label
+End Class
